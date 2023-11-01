986c48dd0cc45bd6147732025aaef07fb57d3c4b
devansh0331 <devanshshrivastava07@gmail.com>
2023-11-01 23:53:35 +0530

    loader

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -47,7 +47,7 @@
     // add form-specific values into the data
     formData.formDataNameOrder = JSON.stringify(fields);
     formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-    formData.formGoogleSendEmail = form.dataset.email || "info@jmrk.in"; // no email by default
+    formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
 
     return { data: formData, honeypot: honeypot };
   }
@@ -67,6 +67,7 @@
 
     disableAllButtons(form);
     var url = form.action;
+    console.log(url);
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url);
     xhr.withCredentials = true;
